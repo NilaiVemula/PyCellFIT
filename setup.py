f9cf916480e7e437d5db5d3ca221f3d5288f5cbd
Nilai Vemula <nilaivemula@gmail.com>
2020-05-14 01:28:13 -0500

    Add testing information to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+#!/usr/bin/env python
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = test_args
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
@@ -13,7 +30,7 @@ setup(
     author='Nilai Vemula',
     author_email='nilai.r.vemula@vanderbilt.edu',
     description='Python implementation of the CellFIT method of inferring cellular forces',
-    # long_description=readme,
+    long_description=readme,
     url='https://github.com/NilaiVemula/PyCellFIT',
     packages=find_packages(),
     install_requires=requirements,
@@ -24,4 +41,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English"
     ],
+    test_suite='tests',
+      cmdclass={'test': PyTest},
 )
