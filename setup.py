df8462ce78884c141485e3494dff12e4d8ed4234
Nilai Vemula <nilaivemula@gmail.com>
2020-05-14 04:35:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='pycellfit',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='Nilai Vemula',
     author_email='nilai.r.vemula@vanderbilt.edu',
