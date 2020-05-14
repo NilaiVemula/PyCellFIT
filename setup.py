73134437b27570cbb599174fde9cf0219c91127f
Nilai Vemula <nilaivemula@gmail.com>
2020-05-14 02:04:11 -0500

    remove testing from setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,6 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-# explicitly config
-test_args = [
-    '--cov-report=term',
-    '--cov-report=html',
-    '--cov=pycellfit',
-    'tests'
-]
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = test_args
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
 
