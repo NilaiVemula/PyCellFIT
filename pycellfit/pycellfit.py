b4827c25351018761a86598dd5dac4ae2d041331
Nilai Vemula <nilaivemula@gmail.com>
2020-05-14 04:32:26 -0500

    Update pycellfit.py

--- pycellfit/pycellfit.py
+++ pycellfit/pycellfit.py
@@ -5,3 +5,11 @@ def hello():
 
     print("Hello World!")
     return "hello new"
+
+def hello2():
+    """
+    simple hello world2 function to test initial commit
+    """
+
+    print("Hello World!2")
+    return "hello new"
