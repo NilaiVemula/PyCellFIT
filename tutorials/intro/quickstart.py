3af1458ff31c8c4a665fb40e78c563338ffe71d5
Nilai Vemula <nilaivemula@gmail.com>
2020-05-20 15:55:30 -0500

    added circle fit and reading in segmentated image files

--- tutorials/intro/quickstart.py
+++ tutorials/intro/quickstart.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-import pycellfit
+
+import matplotlib.pyplot as plt
+
+from pycellfit.utils import read_segmented_image
 
 
 def main():
-    pycellfit.utils.hello()
+    filename = 'hex.tif'
+
+    imgarray = read_segmented_image(filename, visualize=True)
+    plt.show()
+
+    print(imgarray)
 
 
 if __name__ == '__main__':
