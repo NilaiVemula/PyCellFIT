0a865ef7cb6270cdb376773b6ebbde0afe868720
Nilai Vemula <nilaivemula@gmail.com>
2020-06-08 18:55:56 -0500

    trying to fix tests

--- tests/test_segmentation_transform.py
+++ tests/test_segmentation_transform.py
@@ -3,7 +3,6 @@
 import unittest
 
 import numpy as np
-from PIL import Image
 
 import pycellfit.segmentation_transform
 from pycellfit.utils import read_segmented_image
@@ -11,7 +10,7 @@ from pycellfit.utils import read_segmented_image
 
 class TestTransforms(unittest.TestCase):
     """
-        This class contains test cases for the functions in pycellfit/segmentation_tranform.py
+        This class contains test cases for the functions in pycellfit/segmentation_transform.py
     """
 
     def test_skeleton_to_watershed(self):
@@ -31,16 +30,16 @@ class TestTransforms(unittest.TestCase):
 
         hex_skeleton_array = read_segmented_image('tests/images/hex_skeleton.tif')
         watershed_array = pycellfit.segmentation_transform.skeleton_to_watershed(hex_skeleton_array)
-        im = Image.fromarray(watershed_array / 255)
-        im.save('tests/images/hex_watershed.tif')
+        # im = Image.fromarray(watershed_array / 255)
+        # im.save('tests/images/hex_watershed.tif')
         # think of a proper way to test
         self.assertEqual(0, 0)
 
     def test_watershed_to_skeleton(self):
         hex_array = read_segmented_image('tests/images/hex.tif')
         skeleton_array = pycellfit.segmentation_transform.watershed_to_skeleton(hex_array)
-        im = Image.fromarray(skeleton_array)
-        im.save('tests/images/hex_skeleton.tif')
+        # im = Image.fromarray(skeleton_array)
+        # im.save('tests/images/hex_skeleton.tif')
 
         hex_skeleton_array = read_segmented_image('tests/images/hex_skeleton.tif')
 
