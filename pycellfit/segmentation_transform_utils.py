479c4aeddd30c6faecad59dfe97c171c79b5e4ba
Nilai Vemula <nilaivemula@gmail.com>
2020-06-08 18:41:15 -0500

    cleaned up segmentation transform functions, added more testing

--- pycellfit/segmentation_transform_utils.py
+++ pycellfit/segmentation_transform_utils.py
@@ -1,38 +1,5 @@
 import numpy as np
 
-def fill_recursively(array_of_pixels, position, old_value, new_value):
-    """recursive function to replace the value of a pixel and all surrounding pixels to a new value
-
-    :type array_of_pixels: np.ndarray
-    :param array_of_pixels: 2D numpy array of all pixel values
-    :param position: tuple with (row, col) location of pixel to modify
-    :type position: tuple
-    :type old_value: float
-    :param old_value: old value of pixel at `position`
-    :type new_value: float
-    :param new_value: new value for pixel at `position` and surrounding pixels
-    :return: None
-    """
-
-    # Base Case
-    row, col = position
-    if row < 0 or row >= np.shape(array_of_pixels)[0] or col < 0 or col >= np.shape(array_of_pixels)[1]:
-        # out of bounds on the array
-        return
-    elif array_of_pixels[row][col] != old_value or array_of_pixels[row][col] == new_value:
-        # pixel is in different region or has already been modified
-        # pixel value does not need to be modified
-        return
-    else:
-        # change pixel value
-        array_of_pixels[row][col] = new_value
-
-        # recurs for north, east, south, west
-        fill_recursively(array_of_pixels, (row + 1, col), old_value, new_value)
-        fill_recursively(array_of_pixels, (row - 1, col), old_value, new_value)
-        fill_recursively(array_of_pixels, (row, col + 1), old_value, new_value)
-        fill_recursively(array_of_pixels, (row, col - 1), old_value, new_value)
-
 
 def fill_region(array_of_pixels, position, new_value):
     """ fills a region of a 2D numpy array with the same value
@@ -45,9 +12,27 @@ def fill_region(array_of_pixels, position, new_value):
     :param new_value: new value for pixel at `position` and all pixels in same region
     :return: None
     """
-    row, col = position
-    old_value = array_of_pixels[row, col]
-    fill_recursively(array_of_pixels, position, old_value, new_value)
+
+    xsize, ysize = array_of_pixels.shape
+    old_value = array_of_pixels[position[0], position[1]]
+
+    stack = {(position[0], position[1])}
+    if new_value == old_value:
+        raise ValueError("Region is already filled")
+
+    while stack:
+        x, y = stack.pop()
+
+        if array_of_pixels[x, y] == old_value:
+            array_of_pixels[x, y] = new_value
+            if x > 0:
+                stack.add((x - 1, y))
+            if x < (xsize - 1):
+                stack.add((x + 1, y))
+            if y > 0:
+                stack.add((x, y - 1))
+            if y < (ysize - 1):
+                stack.add((x, y + 1))
 
 
 # constant value that the image array is padded with
--- tests/test_segmentation_transform.py
+++ tests/test_segmentation_transform.py
@@ -1,6 +1,5 @@
 """test cases for segmentation_transform functions"""
 
-import sys
 import unittest
 
 import numpy as np
@@ -10,7 +9,7 @@ import pycellfit.segmentation_transform
 from pycellfit.utils import read_segmented_image
 
 
-class TestUtils(unittest.TestCase):
+class TestTransforms(unittest.TestCase):
     """
         This class contains test cases for the functions in pycellfit/segmentation_tranform.py
     """
@@ -21,7 +20,7 @@ class TestUtils(unittest.TestCase):
                           [0, 255, 0, 255, 0],
                           [0, 255, 255, 255, 0],
                           [0, 0, 0, 0, 0]])
-        actual_result = pycellfit.segmentation_transform.skeleton_to_watershed(array)
+        actual_result = pycellfit.segmentation_transform.skeleton_to_watershed(array, keep_boundaries=True)
         print(actual_result)
         expected_result = np.array([[0, 0, 0, 0, 0],
                                     [0, 255, 255, 255, 0],
@@ -29,6 +28,13 @@ class TestUtils(unittest.TestCase):
                                     [0, 255, 255, 255, 0],
                                     [0, 0, 0, 0, 0]])
         self.assertIsNone(np.testing.assert_array_equal(actual_result, expected_result))
+
+        hex_skeleton_array = read_segmented_image('tests/images/hex_skeleton.tif')
+        watershed_array = pycellfit.segmentation_transform.skeleton_to_watershed(hex_skeleton_array)
+        im = Image.fromarray(watershed_array / 255)
+        im.save('tests/images/hex_watershed.tif')
+        # think of a proper way to test
+        self.assertEqual(0, 0)
 
     def test_watershed_to_skeleton(self):
         hex_array = read_segmented_image('tests/images/hex.tif')
@@ -39,15 +45,6 @@ class TestUtils(unittest.TestCase):
         hex_skeleton_array = read_segmented_image('tests/images/hex_skeleton.tif')
 
         self.assertIsNone(np.testing.assert_array_equal(skeleton_array, hex_skeleton_array))
-
-    def test_noboundaries(self):
-        hex_skeleton_array = read_segmented_image('tests/images/hex_skeleton.tif')
-        sys.setrecursionlimit(10 ** 6)
-        watershed_array = pycellfit.segmentation_transform.skeleton_to_watershed(hex_skeleton_array)
-        im = Image.fromarray(watershed_array)
-        im.save('tests/images/hex_watershed.tif')
-
-        self.assertEqual(0, 0)
 
 
 if __name__ == "__main__":
