277550dd32ad402187d428c1993ccac5faf13eba
Nilai Vemula <nilaivemula@gmail.com>
2020-06-12 00:17:18 -0500

    removed obsolete test cases

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -21,42 +21,6 @@ class TestUtils(unittest.TestCase):
         cell_1 = Cell(1)
         self.assertEqual(cell_1.label, 1)
 
-    def test_add_edge_points(self):
-        # Case 1: adds edge points and number of edge points increases
-        cell_0 = Cell(0)
-        cell_membrane = [(0, 0), (0, 1), (1, 0), (1, 1)]
-        for point in cell_membrane:
-            cell_0.add_edge_point(point)
-        self.assertEqual(cell_0.number_of_edge_points, 4)
-
-        # Case 2: doesn't add duplicate points
-        cell_1 = Cell(1)
-        cell_membrane = [(0, 0), (0, 1), (1, 0), (1, 1), (1, 1)]
-        for point in cell_membrane:
-            cell_1.add_edge_point(point)
-        self.assertEqual(cell_1.number_of_edge_points, 4)
-
-    def test_edge_points_cw(self):
-        # Case 1: empty cell should return empty list
-        cell_0 = Cell(0)
-        self.assertEqual(cell_0.edge_points_cw, [])
-
-        # Case 2: sorting should work properly
-        cell_1 = Cell(1)
-        cell_membrane = [(0, 0), (0, 1), (1, 0), (1, 1), (1, 1)]
-        for point in cell_membrane:
-            cell_1.add_edge_point(point)
-        print(type(cell_1.edge_points_cw))
-        self.assertEqual(cell_1.edge_points_cw, [(1, 1), (1, 0), (0, 0), (0, 1)])
-
-    def test_approximate_center(self):
-        # Center of square
-        cell_0 = Cell(0)
-        cell_membrane = [(0, 0), (0, 1), (1, 0), (1, 1)]
-        for point in cell_membrane:
-            cell_0.add_edge_point(point)
-        self.assertEqual(cell_0.approximate_cell_center(), (0.5, 0.5))
-
     def test_eq(self):
         cell_0 = Cell(0)
         cell_1 = Cell(0)
@@ -75,7 +39,7 @@ class TestUtils(unittest.TestCase):
         s.add(Cell(0))
         for cell in s:
             if cell.label == 0:
-                cell.add_edge_point((0, 1))
+                cell.add_edge_point((0, 1), 1)
 
         for cell in s:
             if cell.label == 0:
--- tutorials/intro/quickstart.py
+++ tutorials/intro/quickstart.py
@@ -7,48 +7,30 @@ from pycellfit.utils import read_segmented_image
 
 
 def main():
+    # STEP 1: Reading in segmented image
     filename = 'hex.tif'
     fig, ax = plt.subplots()
     # load input image as a numpy ndarray
-    array_of_pixels = read_segmented_image(filename, visualize=True)
+    array_of_pixels = read_segmented_image(filename)
 
-    # print(img_array)
-
+    # STEP 2: Generate Mesh
     hex_mesh = Mesh(array_of_pixels)
 
     hex_mesh.find_cells_from_array()
 
     print('there are this many cells:')
-
     print(hex_mesh.number_of_cells)
 
     hex_mesh.add_edge_points_and_junctions(array_of_pixels)
 
     print('number of triple junctions:')
     print(hex_mesh.number_of_triple_junctions)
-    for junction in hex_mesh.junctions:
-        junction.plot()
-
-    plt.show()
-
-    # plot the image
-    fig, ax = plt.subplots()
-    plt.imshow(array_of_pixels, cmap='gray', interpolation="nearest")
-
-    for junction in hex_mesh.junctions:
-        junction.plot()
-
-    # plt.xlim(215, 275)
-    # plt.ylim(370, 425)
-    #
-    # ax.set_xticks(np.arange(215, 275, 1), minor=True)
-    # ax.set_yticks(np.arange(370, 425, 1), minor=True)
-    # plt.grid(which='both', alpha=0.2)
-    # plt.show()
 
     hex_mesh.make_edges_for_all_cells()
     print('number of edges')
     print(hex_mesh.number_of_edges)
+
+    # STEP 3: Circle Fit
 
 
 if __name__ == '__main__':
